890b80fb849127cae93e49110a554e20079da6f6
mdjames95 <112733133+mdjames95@users.noreply.github.com>
2022-11-14 22:43:42 -0500

    Assignment Update

---
+++ CIS129FinalProject/Wizert.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace gameLocation;
+
+public static class Wizert 
+{
+   public static userMove()
+   {
+       Console.WriteLine("Which direction would you like to move?");
+       Console.WriteLine("Enter a number: 1.North 2.South 3.East 4.West");
+       var move1 = Console.ReadLine();
+
+       int move2;
+       while(!int.TryParse(move1, out move2)) 
+       {
+           Console.WriteLine("This is not a number.");
+           move1 = Console.ReadLine();
+       }
+       while(move2 != null)
+       {
+           if(move2 == 1)
+           {
+               
+           }
+           else if(move2 == 2)
+           {
+
+           }
+           else if(move2 == 3)
+           {
+
+           }
+           else if(move2 == 4)
+           {
+
+           }
+           else
+           {
+               Console.WriteLine("Make sure you're number is a number between 1-4");
+           }
+       }
+   }
+}
